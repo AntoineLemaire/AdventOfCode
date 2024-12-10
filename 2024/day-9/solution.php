f53812b5a2a74131b7d41b33c14133df14894672
AntoineLemaire <antoine@bouffe.io>
2024-12-10 22:07:30 +0100

    Init days...

---
+++ 2024/day-9/solution.php
@@ -0,0 +1,19 @@
+<?php
+
+class Solution extends AdventOfCode\Solution
+{
+	public function first()
+	{
+		$input = $this->input->load();
+
+		throw new AdventOfCode\Exception\NotImplementedException();
+	}
+
+
+	public function second()
+	{
+		$input = $this->input->load();
+
+		throw new AdventOfCode\Exception\NotImplementedException();
+	}
+}
