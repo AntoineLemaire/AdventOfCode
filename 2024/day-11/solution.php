5bdc5411ba735df6b50935f5ece5144b64984024
AntoineLemaire <antoine@bouffe.io>
2024-12-11 22:25:14 +0100

    Day 11 part 1

--- 2024/day-11/solution.php
+++ 2024/day-11/solution.php
@@ -2,18 +2,71 @@
 
 class Solution extends AdventOfCode\Solution
 {
-	public function first()
-	{
-		$input = $this->input->load();
+    /*
+     * Expected 193607
+     */
+    public function first()
+    {
+        $input = $this->input->load();
+        $stones = explode(' ', $input[0]);
+        $stones = array_map('intval', $stones);
 
-		throw new AdventOfCode\Exception\NotImplementedException();
-	}
 
+        for ($i = 0; $i < 25; ++$i) {
+            $stones = $this->applyRules($stones);
+        }
 
-	public function second()
-	{
-		$input = $this->input->load();
+        return count($stones);
+    }
 
-		throw new AdventOfCode\Exception\NotImplementedException();
-	}
+    public function second()
+    {
+        // Not working
+        throw new AdventOfCode\Exception\NotImplementedException();
+
+        $input = $this->input->load();
+        $stones = explode(' ', $input[0]);
+        $stones = array_map('intval', $stones);
+
+        $count = $this->applyRecursiveRules($stones, 0, 75);
+
+        return $count;
+    }
+
+    private function applyRecursiveRules($stones, int $depth, int $maxDepth)
+    {
+        $count = 0;
+        $stones = $this->applyRules($stones);
+        if ($maxDepth === $depth + 1) {
+            $count += count($stones);
+        } else {
+            foreach ($stones as $stone) {
+                $count += $this->applyRecursiveRules([$stone], $depth + 1, $maxDepth);
+            }
+        }
+
+        return $count;
+    }
+
+    private function applyRules(array $stones): array
+    {
+        $newStones = [];
+        foreach ($stones as $number) {
+            if (0 === $number) {
+                $newStones[] = '1';
+            } elseif (0 === strlen($number) % 2) {
+                $newStones[] = (int) substr($number, 0, strlen($number) / 2);
+                $newStones[] = (int) substr($number, strlen($number) / 2);
+            } else {
+                $newStones[] = $number * 2024;
+            }
+        }
+
+        return $newStones;
+    }
+
+    private function displayStones(array $stones): void
+    {
+        echo implode(' ', $stones) . PHP_EOL;
+    }
 }
